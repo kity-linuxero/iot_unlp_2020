c1239be09dc8084143ecf0afe506ff7e6791a04c
Cristian Giambruni <cgiambruni@gmail.com>
2020-07-01 01:33:49 -0300

    Add InfluxDB support

--- coap_client/iot-coap.py
+++ coap_client/iot-coap.py
@@ -4,17 +4,33 @@ from coapthon.client.helperclient import HelperClient
 import ipaddress
 import time
 import sys
+import requests
 COAP_PORT = 5683
+INFLUX_PORT = '8086'
+DB= "mydb"
 
-def coapmsg (host, path, is_verbose):
+def store_info_influx (host, path, in_influx, value):
+  headers = {'content-type' : 'application/json'}
+  url_string = 'http://'+in_influx+':'+INFLUX_PORT+'/write?db='+DB
+  sensor = path.split('/')[1]
+  data_string = sensor+',host='+str(host)+' '+sensor+'='+str(value)
+  print (url_string+' '+data_string)
+  r = requests.post(url_string,data=data_string)
+  print(r)
+
+def coapmsg (host, path, is_verbose, in_influx):
   try:
     client = HelperClient(server=(str(host), COAP_PORT))
     response = client.get(path).payload
-    if is_verbose:
-      response = client.get(path).pretty_print()
+    if in_influx:
+      store_info_influx(host, path, in_influx, client.get(path).payload)
     else:
-      response = client.get(path).payload
-    print (str(host)+": "+response)
+      if is_verbose:
+        response = client.get(path).pretty_print()
+      else:
+        response = client.get(path).payload
+      print (str(host)+": "+response)
+    
     client.stop()
   except:
       client.stop()
@@ -28,19 +44,20 @@ def clientCoap(hosts, path, interval, in_influx, is_verbose ):
   #host = 'fd00::c30c:0:0:2'
   if interval:
     try:
-      sleeping_time = int(interval)  
+      sleeping_time = int(interval)
       while(1):
         for h in hosts:
-          coapmsg(h, path, is_verbose)
+          coapmsg(h, path, is_verbose, in_influx)
         time.sleep(sleeping_time)
     except KeyboardInterrupt:
       print('Interrupted by keyboard!')
   else:
     try:
       for h in hosts:
-        coapmsg(h, path, is_verbose)
-    except:
-      print ("Exiting with error...")
+        coapmsg(h, path, is_verbose, in_influx)
+    except Exception as err:
+      #print ("Exiting with error: "+str(err))
+      print("pindonga")
 
 # Parseo de arguments
 parser = argparse.ArgumentParser()
@@ -50,7 +67,7 @@ parser.add_argument("-c", "--count", help="cont of motes. IP of motes must be co
 parser.add_argument("-i", "--interval", help="interval in seconds to repeat query", type=int, action="store")
 parser.add_argument("-v", "--verbose", help="increase output verbosity", action="store_true")
 parser.add_argument("-I", "--influx", help="save to influxdb host. Set an ipv4 address", action="store")
-parser.add_argument('--version', action='version', version='%(prog)s 0.1.0')
+parser.add_argument('--version', action='version', version='%(prog)s 0.2.0')
 args = parser.parse_args()
 
 # Validaciones de args
@@ -60,12 +77,12 @@ except ValueError:
   print (args.host+" no es una ip valida")
   sys.exit()
 
-if args.influx:
-  try:
-    ipv4= ipaddress.IPv4Network(args.influx)
-  except ValueError:
-    print (args.influx+" no es una ip valida")
-    sys.exit()
+# if args.influx:
+#   try:
+#     ipv4= ipaddress.IPv4Network(args.influx)
+#   except ValueError:
+#     print (args.influx+" no es una ip valida")
+#     sys.exit()
 # Fin validaciones args
 
 # Por defecto es 1
